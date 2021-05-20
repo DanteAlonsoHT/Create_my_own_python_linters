0713179e3b4ac1acaa6f4075f1976f4fe5adc4bb
DanteAlonsoHT <dante1818@hotmail.com>
2021-05-19 21:00:59 -0500

    Add test/test.py

---
+++ test/test.py
@@ -0,0 +1,23 @@
+#print("Hello World")
+
+print("Hola Mundo")
+
+a= 5
+b =5
+c = [9, 5,3]
+
+
+if True:
+    print("hola")
+    if True:
+        print("Hola")
+        if True:
+          print("Hola")
+
+while True :
+  print ( "Hola" )
+  break
+
+print ( a + b )
+
+print ("Hola mundo")
