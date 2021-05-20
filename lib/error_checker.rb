d330e716f2cd162c5572a7afb87352a7e3eb3f65
DanteAlonsoHT <dante1818@hotmail.com>
2021-05-19 21:09:11 -0500

    Create check_indentation method

--- lib/error_checker.rb
+++ lib/error_checker.rb
@@ -7,4 +7,18 @@ class ErrorChecker < FileLector
         @empty_spaces_counter = 0
         @error_message = ""
     end
+
+    def check_indentation
+        @line.split(//).each do |likely_space|
+            @empty_spaces_counter +=1 if likely_space == " "
+            @empty_spaces_counter = 0 if ((@empty_spaces_counter == 1) && (likely_space != " "))
+            @empty_spaces_counter = 0 if ((@empty_spaces_counter % 4 == 0) && (likely_space != " "))
+        end
+        if @empty_spaces_counter == 1
+            @error_message.concat("#{lines_counter}:1 Expected indentation of 0 spaces\n")
+        elsif @empty_spaces_counter % 4 != 0
+            @error_message.concat("#{lines_counter}:1 Expected indentation of 4 spaces\n")
+            @error_message.concat("#{@empty_spaces_counter} space(s) found\n")
+        end
+    end
 end
