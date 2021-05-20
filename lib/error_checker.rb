c90c9559c5c4691692d56256a3fa3e05bbb3ab8e
DanteAlonsoHT <dante1818@hotmail.com>
2021-05-19 21:10:10 -0500

    Create check_errors method

--- lib/error_checker.rb
+++ lib/error_checker.rb
@@ -21,4 +21,55 @@ class ErrorChecker < FileLector
             @error_message.concat("#{@empty_spaces_counter} space(s) found\n")
         end
     end
+
+    def check_errors
+        if @line.match(/\w=/)
+            @error_message.concat("#{@lines_counter}:#{(@line =~ /\w=/) + 1} Expected whitespace before '='\n")
+            @error_message.concat("\s\n#{@line}\n")
+            (@line =~ /\w=/).to_i.times { |i| @error_message.concat("\s") }
+            @error_message.concat("\s^\n")
+        end
+        if @line.match(/=\w/)
+            @error_message.concat("#{@lines_counter}:#{(@line =~ (/=\w/)) + 1} Expected whitespace after '='")
+            @error_message.concat("\s\n#{@line}\n")
+            (@line =~ (/=\w/)).to_i.times { |i| @error_message.concat("\s") }
+            @error_message.concat("\s^\n")
+        end
+        if @line.match(/[a-z]\s:/)
+            @error_message.concat("#{@lines_counter}:#{(@line =~ /[a-z]\s:/) + 1} Unexpected whitespace before ':'")
+            @error_message.concat("\s\n#{@line}\n")
+            (@line =~ /[a-z]\s:/).to_i.times { |i| @error_message.concat("\s") }
+            @error_message.concat("\s^\n")
+        end
+        if @line.match(/:\s\s/)
+            @error_message.concat("#{@lines_counter}:#{(@line =~ /:\s\s/) + 1} Unexpected whitespace after ':'")
+            @error_message.concat("\s\n#{@line}\n")
+            (@line =~ /:\s\s/).to_i.times { |i| @error_message.concat("\s") }
+            @error_message.concat("\s^\n")
+        end
+        if @line.match(/,\w/)
+            @error_message.concat("#{@lines_counter}:#{(@line =~ /,\w/) + 1} Missing whitespace after ','")
+            @error_message.concat("\s\n#{@line}\n")
+            (@line =~ /,\w/).to_i.times { |i| @error_message.concat("\s") }
+            @error_message.concat("\s^\n")
+        end
+        if @line.match(/\w\s\s+/)
+            @error_message.concat("#{@lines_counter}:#{(@line =~ /\w\s\s+/) + 1} multiple spaces after keyword")
+            @error_message.concat("\s\n#{@line}\n")
+            (@line =~ /\w\s\s+/).to_i.times { |i| @error_message.concat("\s") }
+            @error_message.concat("\s^\n")
+        end
+        if @line.match(/print\s+/)
+            @error_message.concat("#{@lines_counter}:#{(@line =~ /t\s+/)} Unexpected whitespace(s) after 'print'")
+            @error_message.concat("\s\n#{@line}\n")
+            (@line =~ /t\s+/).to_i.times { |i| @error_message.concat("\s") }
+            @error_message.concat("\s^\n")
+        end
+        if @line.match(/#\w/)
+            @error_message.concat("#{@lines_counter}:#{(@line =~ (/#\w/))} Expected whitespace after '#' for comments")
+            @error_message.concat("\s\n#{@line}\n")
+            (@line =~ /#\w/).to_i.times { |i| @error_message.concat("\s") }
+            @error_message.concat("\s^\n")
+        end
+    end
 end
