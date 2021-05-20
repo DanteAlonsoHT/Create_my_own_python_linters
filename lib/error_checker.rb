ce94cb792d48e57af67393288a7254404b1254e1
DanteAlonsoHT <dante1818@hotmail.com>
2021-05-19 21:07:35 -0500

    Create new_line method

--- lib/error_checker.rb
+++ lib/error_checker.rb
@@ -1,4 +1,10 @@
 require_relative 'reader'
 
 class ErrorChecker < FileLector
+    def new_line(line, lines_counter)
+        @line = line
+        @lines_counter = lines_counter
+        @empty_spaces_counter = 0
+        @error_message = ""
+    end
 end
